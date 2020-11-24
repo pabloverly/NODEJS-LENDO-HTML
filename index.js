6cd17ecb71fee2d30fa920efe8fd503be3a318aa
pabloverly <pablopierre@gmail.com>
2020-11-24 07:15:58 -0300

    atualizando 24-11-2020

--- index.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-    $(document).on('click', '.sD', function(e) {
-        e.preventDefault;
-        var nome = $(this).closest('tr').find('td[data-nome]').data('nome');
-        alert(nome);
-    });
-});
